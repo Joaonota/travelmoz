0fdcb855eb1b5ed1c30dcef34e6dbd8aca9d518b
joaonota <joaonota23@gmail.com>
2022-02-15 06:45:31 +0200

    actualização

---
+++ lib/prereload/prereload.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Prereload extends StatefulWidget {
+  const Prereload({ Key? key }) : super(key: key);
+
+  @override
+  _PrereloadState createState() => _PrereloadState();
+}
+
+class _PrereloadState extends State<Prereload> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
