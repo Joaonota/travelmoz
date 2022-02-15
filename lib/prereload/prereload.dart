b1de0b0fe77a6d2b3746bee64ed0c4948a749e65
joaonota <joaonota23@gmail.com>
2022-02-15 07:25:48 +0200

    nova actualização

--- lib/prereload/prereload.dart
+++ lib/prereload/prereload.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Prereload extends StatefulWidget {
-  const Prereload({ Key? key }) : super(key: key);
+  const Prereload({Key? key}) : super(key: key);
 
   @override
   _PrereloadState createState() => _PrereloadState();
@@ -10,8 +10,28 @@ class Prereload extends StatefulWidget {
 class _PrereloadState extends State<Prereload> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.center,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color(0xfff8f9fa),
+              Color(0xfff8f9fa),
+            ])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/logo/logos.png",
+                width: 300,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
