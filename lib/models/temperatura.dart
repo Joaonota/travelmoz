3f19873bca23249666f9ff7ca4e8b90da45bea20
joaonota <joaonota23@gmail.com>
2022-03-24 22:05:46 +0200

    api de temperatura funcionado

--- lib/models/temperatura.dart
+++ lib/models/temperatura.dart
@@ -1,1 +1,16 @@
-G
+class Temperatura {
+  final double? tempmax;
+  final double? tempmin;
+
+  const Temperatura({
+    required this.tempmax,
+    required this.tempmin,
+  });
+
+  factory Temperatura.fromJson(Map<String, dynamic> json) {
+    return Temperatura(
+      tempmax: json['temp_max'],
+      tempmin: json['temp_min'],
+    );
+  }
+}
